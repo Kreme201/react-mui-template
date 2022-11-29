dccc116947b3b6e0d7f250eb442c80e255c52c74
kreme201 <kreme201@gmail.com>
2022-11-29 10:59:32 +0900

    코드 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Button } from "@mui/material";
+
+export default function App() {
+    const handleClick = () => {
+        alert("Hello World");
+    };
+    return (
+        <div>
+            <Button variant="contained" onClick={handleClick}>
+                Hello World
+            </Button>
+        </div>
+    );
 }
-
-export default App;
